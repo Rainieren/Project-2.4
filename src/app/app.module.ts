eab6be8a64b22fe5dcbd128f0dc8625cad611877
Joost Blaauwwiekel <keeperjoost@gmail.com>
2020-05-28 16:19:22 +0200

    Made a PWA

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,8 @@ import { HeaderComponent } from './header/header.component';
 import { TablesComponent } from './main/tables/tables.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { TableCardComponent } from './main/table-card/table-card.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -21,7 +23,8 @@ import { TableCardComponent } from './main/table-card/table-card.component';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    NgbModule 
+    NgbModule,
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }) 
   ],
   providers: [
   ],
