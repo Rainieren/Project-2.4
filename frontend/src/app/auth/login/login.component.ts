18be5896b3434417727d64b39ab2d3275ac0b9a4
jappe193 <jaspervandijken1999@hotmail.com>
2020-06-03 17:39:52 +0200

    Simple login form

--- frontend/src/app/auth/login/login.component.ts
+++ frontend/src/app/auth/login/login.component.ts
@@ -1,16 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-login',
-  templateUrl: './login.component.html',
-  styles: [
-  ]
+  templateUrl: './login.component.html'
 })
-export class LoginComponent implements OnInit {
 
-  constructor() { }
+//TODO: remove OnInit?
+export class LoginComponent {
+  public username: string;
+  public password: string;
+  public errorMsg: string;
 
-  ngOnInit(): void {
+  constructor(private auth: AuthService, private router: Router) {};
+
+  
+  onSubmit() {
+    //Call auth.login, act on result
+    this.auth.login(this.username, this.password)
+    .subscribe(
+      result => this.router.navigate(['dashboard']),
+      err => this.errorMsg = 'Uw gebruikersnaam of wachtwoord is incorrect...'
+    );
+      
+    console.log(this.username);
+    console.log(this.password);
+
   }
+  
 
 }
